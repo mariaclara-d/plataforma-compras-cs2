af587e7f5c28437da3286cd972106ce54e5905d3
Maria Clara Guimarães <mariaclaradesenvolve@gmail.com>
2025-01-30 18:04:01 -0300

    banco de dados

--- app.py
+++ app.py
@@ -1,40 +1,40 @@
 from flask import Flask
+from db_config import db
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
+app.config['STEAM_API_KEY_NAO_OFICIAL'] = os.getenv("STEAM_API_KEY_NAO_OFICIAL")
+app.config['STEAM_API_KEY'] = os.getenv("STEAM_API_KEY")
+app.config['STEAM_RETURN_URL'] = os.getenv("STEAM_RETURN_URL")
+app.config['STEAM_REALM'] = os.getenv("STEAM_REALM")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SQLALCHEMY_DATABASE_URI")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 from routes.home import home_blueprint
 from routes.auth import auth_blueprint
 from routes.dashboard import dashboard_blueprint
 from routes.forms import forms_blueprint
 from routes.inventory import inventory_blueprint
 from routes.trade import trade_blueprint
-from dotenv import load_dotenv
-import os 
+from routes.offer import offer_blueprint
 
-load_dotenv()
+db.init_app(app)
 
-
-# Inicialização do Flask
-app = Flask(__name__)
-
-app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
-app.config['STEAM_API_KEY_NAO_OFICIAL']=os.getenv("STEAM_API_KEY_NAO_OFICIAL")
-app.config['STEAM_API_KEY']=os.getenv("STEAM_API_KEY")
-app.config['STEAM_RETURN_URL']=os.getenv("STEAM_RETURN_URL")
-app.config['STEAM_REALM']=os.getenv("STEAM_REALM")
-
-# Registro de blueprints
 app.register_blueprint(home_blueprint)
 app.register_blueprint(auth_blueprint)
 app.register_blueprint(dashboard_blueprint)
-
 app.register_blueprint(forms_blueprint, url_prefix='/forms')
 app.register_blueprint(inventory_blueprint, url_prefix='/inventory')
 app.register_blueprint(trade_blueprint, url_prefix='/trade')
-
+app.register_blueprint(offer_blueprint, url_prefix='/offer')
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5000)
-
-
-
 
 
 
